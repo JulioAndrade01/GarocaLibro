82f62b1c621a60e9f66f0c736ba44d2710c2e6f6
JulioAndrade01 <167430558+JulioAndrade01@users.noreply.github.com>
2024-11-08 16:27:32 -0300

    Ajuste

--- library_manager/settings.py
+++ library_manager/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Configuração de segurança
 SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'  # Alterado para usar variável de ambiente
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = ['garoca1-3d0d78d257fa.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Configuração dos apps do Django
@@ -21,24 +21,20 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'bootstrap5',
-    'storages',  # django-storages para integração com AWS S3
-    'csp',  # Adicionado para Content Security Policy (se for necessário)
+    'storages',
 ]
 
 # Configuração do middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # WhiteNoise para arquivos estáticos
-    'django.middleware.http.ConditionalGetMiddleware',  # Middleware condicional para cache
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
-
-# Se não for necessário CSP, remova a linha abaixo
-# 'csp.middleware.CSPMiddleware', 
 
 # Configuração de URLs e WSGI
 ROOT_URLCONF = 'library_manager.urls'
@@ -72,28 +68,25 @@ DATABASES = {
     }
 }
 
-# Configuração do banco de dados no Heroku usando JAWSDB_URL
 ja_database_url = os.getenv('JAWSDB_URL')
 if ja_database_url:
     DATABASES['default'].update(dj_database_url.config(default=ja_database_url))
-else:
-    # Banco de dados local (apenas para DEBUG = True)
-    if DEBUG:
-        DATABASES['default'].update({
-            'NAME': 'DJANGO_G',
-            'USER': 'djangoadmin',
-            'PASSWORD': '100902',
-            'HOST': 'localhost',
-            'PORT': '3306',
-            'OPTIONS': {
-                'charset': 'utf8mb4',
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-            }
-        })
+elif DEBUG:
+    DATABASES['default'].update({
+        'NAME': 'DJANGO_G',
+        'USER': 'djangoadmin',
+        'PASSWORD': '100902',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    })
 
 DATABASES['default']['CONN_MAX_AGE'] = 600
 
-# Configuração de cache (apenas para ambiente local)
+# Configuração de cache
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
@@ -101,10 +94,8 @@ CACHES = {
     }
 }
 
-# Configuração de sessão
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
-# Validação de senha
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -112,31 +103,21 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-# Configurações de internacionalização e fuso horário
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
-# Configuração de arquivos estáticos
 STATIC_URL = '/static/'
-
-# Diretório para os arquivos estáticos gerados
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-# Configuração do Whitenoise para arquivos estáticos
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Adicionando cabeçalhos de cache
 WHITENOISE_MAX_AGE = 31536000  # 1 ano de cache
 
-# Configuração de arquivos de mídia
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 if not DEBUG:
-    # Configuração para Amazon S3
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
@@ -146,57 +127,45 @@ if not DEBUG:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
-# Configuração para campo padrão de chaves primárias
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Codificação padrão
 DEFAULT_CHARSET = 'utf-8'
 
-# Configuração do usuário personalizado
 AUTH_USER_MODEL = 'core.Leitor'
 
-# Configuração de autenticação e redirecionamento
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/perfil/'
 LOGOUT_REDIRECT_URL = '/login/'
 
-# Segurança dos cookies e HTTPS
 SESSION_COOKIE_SECURE = not DEBUG
 CSRF_COOKIE_SECURE = not DEBUG
-SECURE_SSL_REDIRECT = not DEBUG  # Redireciona HTTP para HTTPS
-SECURE_HSTS_SECONDS = 31536000 if not DEBUG else 0  # 1 ano de segurança
+SECURE_SSL_REDIRECT = not DEBUG
+SECURE_HSTS_SECONDS = 31536000 if not DEBUG else 0
 SECURE_HSTS_INCLUDE_SUBDOMAINS = not DEBUG
 SECURE_HSTS_PRELOAD = not DEBUG
 
-
-# Cabeçalhos de segurança
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
-# Middleware para adicionar cabeçalho Cache-Control simplificado
+# Middleware para controle de cache personalizado
 from django.utils.cache import patch_cache_control
 from django.utils.deprecation import MiddlewareMixin
 
 class CacheControlMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        # Evita cache na página de login
-        if request.path.startswith('/login/'):
+        if request.path.startswith('/login/') or request.path.startswith('/perfil/'):
+            # Evita cache em páginas dinâmicas de login e perfil
             patch_cache_control(response, no_cache=True, no_store=True, must_revalidate=True)
-        else:
-            # Para outras páginas, permite cache
-            patch_cache_control(response, public=True, max_age=86400)  # 1 dia de cache
-
-        # Cabeçalhos para arquivos estáticos
-        if request.path.startswith('/static/'):
+        elif request.path.startswith('/static/'):
+            # Cache prolongado para arquivos estáticos
             patch_cache_control(response, public=True, max_age=WHITENOISE_MAX_AGE, immutable=True)
+        else:
+            # Cache curto para outras páginas
+            patch_cache_control(response, public=True, max_age=180)
 
         return response
 
-
-# Adiciona ao final da lista de middlewares
 MIDDLEWARE.append('library_manager.settings.CacheControlMiddleware')
 
-# Logging Configurations
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
