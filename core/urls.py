45ee467b65dbf452e0952cd050292b927b755ccd
JulioAndrade01 <167430558+JulioAndrade01@users.noreply.github.com>
2024-11-13 16:36:18 -0300

    importaçoes e correçoes

--- core/urls.py
+++ core/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
     path('livros/view/', livros_view, name='livros-view'),
     #path('perfil/', perfil_view, name='perfil'),
     path('meu_perfil/', meu_perfil_view, name='meu_perfil'),
-    path('login/', login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('register/', register, name='register'),
     path('agendar-retirada/', agendar_retirada, name='agendar_retirada'),
