c98a976f28aee71a1e9a9987cf58d5f76fd0bd28
Mauro Ue <mauroue@gmail.com>
2025-05-15 18:33:29 -0300

    fix(manage): create super user issue

--- core/models.py
+++ core/models.py
@@ -52,6 +52,20 @@ class Leitor(Base, AbstractBaseUser, PermissionsMixin):
 
     objects = LeitorManager()
 
+    @classmethod
+    def normalize_email(cls, email):
+        """
+        Normalize the email address by lowercasing the domain part of it.
+        """
+        email = email or ""
+        try:
+            email_name, domain_part = email.strip().rsplit("@", 1)
+        except ValueError:
+            pass
+        else:
+            email = email_name.lower() + "@" + domain_part.lower()
+        return email
+
     def has_perm(self, perm, obj=None):
         return self.is_superuser
 
